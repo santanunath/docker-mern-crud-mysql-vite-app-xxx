e95e5fbf2c0bd543c8661884cd4967287cde465d
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-04 22:24:10 +0530

    Update ConnectDB.js

--- backend/db/ConnectDB.js
+++ backend/db/ConnectDB.js
@@ -44,7 +44,7 @@ const ConnectDB = async () => {
   // ------------------
   await pool.query(`CREATE TABLE IF NOT EXISTS \`${process.env.DB_TABLENAME}\` (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
+        name  VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`);
