7208d5cafcdd90b74c51a99a788b823807efae55
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-04 22:31:08 +0530

    Update ConnectDB.js

--- backend/db/ConnectDB.js
+++ backend/db/ConnectDB.js
@@ -1,1 +1,27 @@
+// importing the mysql2/promise 
+// for using 'async' 'await' in conenction function and in querys
+// --------------
+const mysql = require("mysql2/promise");
 
+// --------------------
+// the 'async' 'await' function 
+// which connects to the database 
+// using the credentials in the .env files
+//---------------------
+const ConnectDB = async () => {
+  
+  const pool = await mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: process.env.DB_WAITFORCONNECTIONS,
+    connectionLimit: process.env.DB_CONNECTIONLIMIT,
+    queueLimit: process.env.DB_QUEUELIMIT
+  }
+                                      
+
+// ----------------
+// exporting the function
+// ----------------
+module.exports = ConnectDB;
